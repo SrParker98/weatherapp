2da4deae4fa7efb18df7917556cb305092a0d6df
sergi navarro <116755291+SrParker98@users.noreply.github.com>
2023-05-03 11:48:41 +0200

    Update Weathermap.jsx

--- app-weather/weatherapp/src/components/Weathermap.jsx
+++ app-weather/weatherapp/src/components/Weathermap.jsx
@@ -3,13 +3,10 @@ import Form from './Form'
 import React, { useState } from 'react';
 import Card from './Card';
 
-
  const Weathermap =()=> {
   let Url = "https://api.openweathermap.org/data/2.5/weather?&appid=fd31cecaca6e39cc8080eb72cbda17b6&lang=es";
   let cityUrl = "&q=";
   let forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?&appid=fd31cecaca6e39cc8080eb72cbda17b6&lang=es";
-  
-
 
   const [weather, setWeather]= useState([]);
   const [forecast, setForecast]= useState([]);
@@ -18,16 +15,17 @@ import Card from './Card';
   const getLocation = async (loc)=>{
 
     setLocation(loc)
-    
-   Url = Url + cityUrl + loc;
+    Url = Url + cityUrl + loc;
     
     await fetch(Url).then((res)=>{
       
       if(!res.ok)throw{res}
       return res.json();
     }).then((weatherData)=>{
+     
       console.log(weatherData)
         setWeather(weatherData);
+     
       });
 
       forecastUrl = forecastUrl + cityUrl + loc;
@@ -43,23 +41,20 @@ import Card from './Card';
       
   }
  
- 
   return (
     
     <React.Fragment>
-      <Form
-      newLocation= {getLocation}
-      />
+       <Form
+        newLocation= {getLocation}
+       />
+
       <Card
       weather={weather}
       forecast={forecast}
       />
+       
     </React.Fragment>
-    
-    
   );
-  
-  
 }
 export default Weathermap;
 
